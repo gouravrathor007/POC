2799986c0cc38d2478fbbb8d4a021d1ed3d76d30
gouravrathor007 <gouravrathor@gmail.com>
2020-05-13 21:13:39 +0530

    update

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -3,19 +3,20 @@ import { AuthenticationService, UserService, AlertService } from '../_services';
 import { AlertStubService } from './../_stubs/alert-stub.service';
 import { AuthenticationStubService } from './../_stubs/authentication-stub.service';
 import { UserStubService } from './../_stubs/user-stub.service';
-import { RegisterComponent } from './register.component';
+import { LoginComponent } from './login.component';
+import { Router } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
-describe('RegisterComponent', () => {
-  let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
   let authenticationService: AuthenticationService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ],
+      declarations: [ LoginComponent ],
       providers: [FormBuilder,
         {
             provide: AuthenticationService,
@@ -36,14 +37,12 @@ describe('RegisterComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RegisterComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     authenticationService = fixture.componentRef.injector.get(AuthenticationService);
-    component.registerForm = new FormBuilder().group({
-        firstName: [],
-        lastName: [],
-        username: [],
-        password: []
+    component.loginForm = new FormBuilder().group({
+        username:[],
+        password:[]
     });
   });
   it('should createcomponent and redirect to home if already logged in', () => {
